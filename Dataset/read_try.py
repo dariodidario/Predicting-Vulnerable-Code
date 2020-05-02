5c7ceb33b8fb514d7aeb86f19cfa6f236aac5fb2
Dario Di Dario <dariodidario97@gmail.com>
2020-05-02 19:41:49 +0200

    UPDATE read_try
    Managed the error to mine the repo.

--- Dataset/read_try.py
+++ Dataset/read_try.py
@@ -5,7 +5,7 @@ from pydriller import RepositoryMining
 
 def initialize():
     cwd=os.getcwd()
-    with open('papero_2.csv', mode='r') as csv_file:
+    with open('papero_2_prova.csv', mode='r') as csv_file:
         if "RepositoryMining" not in os.listdir():
              os.mkdir("RepositoryMining")
         os.chdir("RepositoryMining")
@@ -18,7 +18,12 @@ def initialize():
             i+=1
         startMiningRepo(data, cwd)
 
-def startMiningRepo(data, cwd):    
+def startMiningRepo(data, cwd):
+    statusOK = "OK!\n"
+    statusNE = "NOT EXIST COMMIT\n"
+    statusNR = "REPO NOT AVAILABLE\n"
+    file1 = open("CHECK.txt", "a")
+    j = 0    
     for line in data:
         link=data[line]['repolink']+'.git'
         #per chiamare la seconda api e controllare che il commit esiste
@@ -26,6 +31,8 @@ def startMiningRepo(data, cwd):
         commit_id=data[line]['commit_id']
         cve_id=data[line]['cve_id']
         print(link)
+        status = ""
+        toWrite = "indice: " + str(j+1) + " link repo: " + str(link1) + " status: "
         response = requests.get(link)
         if response:
             response1 = requests.get(link1+"/commit/"+commit_id)
@@ -43,14 +50,23 @@ def startMiningRepo(data, cwd):
                              os.chdir(commit_id)
                              if mod.source_code_before != None:
                              	javafile=open(mod.filename,"w+")
-                             	print(mod.source_code_before)
                              	javafile.write(mod.source_code_before)
                              os.chdir(cwd+"/RepositoryMining")
+                    status = statusOK
+                    toWrite = toWrite + status
+                    file1.write(toWrite)
+                    j+=1
             else:
-                print("Commit non esistente!")       
+                status = statusNE
+                toWrite = toWrite + status
+                file1.write(toWrite)
+                j+=1    
         else:
-            print("Repository non esistente o privata")
-
+            status = statusNR
+            toWrite = toWrite + status
+            file1.write(toWrite)
+            j+=1
+    file1.close()
 def main():
     initialize()
 if __name__ == '__main__':
