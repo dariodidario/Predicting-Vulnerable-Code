5f3e693a6454818ac228b604d391894819e15134
gilrec <gilbertrecupito@gmail.com>
2020-05-14 23:49:32 +0200

    String constant and comments check for text mining

--- Dataset/text_mining.py
+++ Dataset/text_mining.py
@@ -1,7 +1,22 @@
 import re
 
 def stringTokenizer(s):
+	#Take the string constant values
+    strings_values= re.findall(r'\"(.+?)\"', s)
+    #Take the comments
+    comments= re.findall(r'\*(.+?)\*', s)
+    #Put the comments and the string values in a single list
+    discard_list= strings_values+comments
+    print(strings_values)
+    print(comments)
     tokens = re.findall(r"[\w']+|[^\w\s']", s)
+    #split the discard_list in words
+    for string in discard_list:
+        word_vector=string.split(" ")
+        #remove each word in tokens
+        for word in word_vector:
+           if word in tokens:
+              tokens.remove(word)
     withoutAlpha = removeNotAlpha(tokens)
     return withoutAlpha
 
@@ -17,7 +32,7 @@ def removeNotAlpha(tokens):
 		if(element.isalpha()):
 			rightOne.append(element)
 	return rightOne
-def main():
+def main():	
 	takeJavaClass()
 
 if __name__ == '__main__':
