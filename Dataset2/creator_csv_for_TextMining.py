6c4cf337df8f4d7bdd764b69d1d39113d5c1214c
gilbertrec <gilbertrecupito@gmail.com>
2020-06-23 11:48:50 +0200

    Added splitting CamelCase feature for csv_creator

--- Dataset2/creator_csv_for_TextMining.py
+++ Dataset2/creator_csv_for_TextMining.py
@@ -1,9 +1,11 @@
 import ast
 import os
 import random
+import less_element_text_mining #CamelCase splitting module
+
 def main():
 	dict_file_name="text_mining_dict.txt"
-	final_csv_name = "csv_mining_final.txt"
+	final_csv_name = "csv_mining_final.csv"
 	cwd = os.getcwd()
 	repo_name = "RepositoryMining"
 	os.chdir(cwd+"/mining_results")
@@ -35,9 +37,10 @@ def main():
 											current_file = open(file, "r")
 											#converto il dizionario piccolo in un dic
 											current_dict = ast.literal_eval(current_file.read())
+											current_dict = less_element_text_mining.splitCamelCase(current_dict) #splitting the current dict of the java class in more words by CamelCase
 											name_of_file_for_csv = file.replace("text_mining.txt", "")
 											final_csv.write(name_of_file_for_csv)
-											for element in csv_sorted: 
+											for element in csv_sorted:
 												if element in current_dict.keys():
 													final_csv.write(",")
 													final_csv.write(str(current_dict[element]))	
